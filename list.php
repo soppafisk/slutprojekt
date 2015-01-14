54a52373e1eb500a7c477136390eb86828412fc3
smurfen <hej@jenserik.se>
2015-01-14 16:07:01 +0100

    påbörjan till  paginering. Nu går jag hem.

--- list.php
+++ list.php
@@ -4,12 +4,14 @@
 	<h1>Lista/index</h1>
 
 	<?php
-
+	$postCount = sqlQuery("SELECT COUNT(*) FROM posts")[0]["COUNT(*)"];
+	$postsPerPage = 3;
+	$pages = ceil($postCount / $postsPerPage);
+	print $pages;
 	$result = sqlQuery("SELECT posts.*, users.username FROM `posts` JOIN users on posts.user_id = users.id");
 
 	foreach ($result as $post) {
 		include "incl/postbox.php";
 	}
 	?>
-
 </div>

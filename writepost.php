cbb20e9e3c106823f9a69ee9acacf3bd879fb19b
smurfen <hej@jenserik.se>
2015-02-09 13:40:52 +0100

    nytt inlägg har booot

--- writepost.php
+++ writepost.php
@@ -1,6 +1,6 @@
 <?php require_once "incl/header.php"; ?>
 
-<div class="col-8">
+<div class="col-xs-8">
 <?php if (isLoggedIn()): 
 	$categories = sqlQuery("SELECT * FROM categories");
 	$currentCat = 0;
@@ -8,34 +8,44 @@
 
 	<h1>Skriv inlägg</h1>
 	<form action="posts/postsend.php" method="POST">
+		<div class="formgroup">
 		<label for="category">Kategori:<br></label>
-		<select name="category">
-			<option value="0"> </option>
-			<?php 
-				foreach ($categories as $category) {
-					$selected = false;
-					if (isset($_GET['cat'])) {
-						if (in_array($_GET['cat'], $category)) {
-							$currentCat = $category;
-							$selected = true;
+
+			<select name="category" class="form-control">
+				<option value="0"> </option>
+				<?php 
+					foreach ($categories as $category) {
+						$selected = false;
+						if (isset($_GET['cat'])) {
+							if (in_array($_GET['cat'], $category)) {
+								$currentCat = $category;
+								$selected = true;
+							}
 						}
+						print "<option value='{$category['id']}'";
+						if ($selected) 
+							print " selected ";
+						print ">{$category['fullName']}</option>";
 					}
-					print "<option value='{$category['id']}'";
-					if ($selected) 
-						print " selected ";
-					print ">{$category['fullName']}</option>";
-				}
-			?>
-		</select><br>
-		<label for="title">Titel:<br></label>
-		<input type="text" name="title">
-		<label for="link">Länk: (glöm inte http://)<br></label>
-		<input type="text" name="link">
-		<label for="content">Beskrivning:<br></label>
-		<textarea name="content" cols="30" rows="10"></textarea>
-		<label for="nsfw"><abbr title="Not Safe For Work">NSFW</abbr>: </label>
-		<input type="checkbox" name="nsfw"><br>
-		<input type="submit" value="Skicka">
+				?>
+			</select>
+		</div>
+		<div class="formgroup">
+			<label for="title">Titel:</label>
+			<input type="text" class="form-control" name="title">
+		</div>
+		<div class="formgroup">
+			<label for="link">Länk: (glöm inte http://)</label>
+			<input type="text" class="form-control" name="link">
+		</div>
+		<div class="formgroup">
+			<label for="content">Beskrivning:</label>
+			<textarea name="content" cols="30" rows="10" class="form-control"></textarea>
+		</div>
+		<div class="checkbox">
+			<label><input type="checkbox" name="nsfw"><abbr title="Not Safe For Work">NSFW</abbr></label>
+		</div>
+		<input type="submit" class="btn btn-default" value="Skicka">
 	</form>
 
 <?php else: ?>
